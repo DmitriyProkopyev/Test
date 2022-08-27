fd253ab4fd3231ce5425a0913b8209f57e77c7fd
BatDen52 <kaluzhin.denis@gmail.com>
2022-08-27 14:23:30 +0300

    Добавил лист юнитов в зону

--- Assets/Game/Sources/Environment/Zone.cs
+++ Assets/Game/Sources/Environment/Zone.cs
@@ -5,4 +5,26 @@ using UnityEngine;
 
 public abstract class Zone : MonoBehaviour
 {
+    private List<Unit> _units = new List<Unit>();
+
+    public void AddUnit(Unit unit)
+    {
+        _units.Add(unit);
+    }
+
+    public void AddUnit(List<Unit> units)
+    {
+        _units.AddRange(units);
+    }
+
+    public IEnumerable<Unit> TakeAllUnits()
+    {
+        if (_units.Count == 0)
+            return null;
+
+        IEnumerable<Unit> units = new List<Unit>(_units);
+        _units.Clear();
+
+        return units;
+    }
 }
--- Assets/Game/Sources/Environment/ZonePaddock.cs
+++ Assets/Game/Sources/Environment/ZonePaddock.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 
 public class ZonePaddock : Zone
 {
+    [SerializeField] private int _price;
 }
