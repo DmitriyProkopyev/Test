4bb6584b9e78604887a199037941568ce0364bbb
Kirill <kglad67@gmail.com>
2022-08-27 13:48:35 +0300

    add PlayerMove.cs

---
+++ Assets/Game/Sources/Logic/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Zenject;
+
+namespace Game.Sources
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        private IInputService _inputService;
+
+        [Inject]
+        public void Constructor(IInputService inputService)
+        {
+            _inputService = inputService;
+        }
+
+        public void Update()
+        {
+            transform.position += new Vector3(_inputService.Delta.x, 0, _inputService.Delta.y);
+        }
+    }
+}
